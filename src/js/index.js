86ad9a6fb5af8b5a16758613032539be356596fd
Ilija <omniiknight@hotmail.com>
2018-11-09 10:02:07 +0100

    Adding loader and pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 // global state of the app
 const state = {}
@@ -16,12 +16,15 @@ const controlSearch = async () => {
         // 3 Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4  Search for recipes
         await state.search.getResults();
 
         // 5 render results on ui
+        clearLoader();
         searchView.renderResults(state.search.result);
+
     }
 }
 
@@ -29,4 +32,15 @@ elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch();
 
+});
+
+
+elements.searchResPages.addEventListener("click", e => {
+    const btn = e.target.closest(".btn-inline");
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+
+    }
 });
